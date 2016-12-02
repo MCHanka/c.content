2b2b12ab376e2c3cc2b9a17bbf232b505a95f8f6
IGSteven <me@igsteven.com>
2016-12-02 13:04:40 +0000

    Create tools.php

---
+++ page/tools.php
@@ -0,0 +1,121 @@
+<div align="center" class="hft">
+	<div>
+		<h1><b>Helpful tools</b></h1>
+		<h3>Streaming Software</h3>
+		<ul>
+			<li>
+				<h2>OBS</h2>
+				<p>
+					Open Broadcaster Software is free and open source software for video recording and live streaming.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=obsproject.com">GET OBS</a>
+			</li>
+			<li>
+				<h2>Xplit</h2>
+				<p>
+					Revolutionary audio/video mixer that allows for endless customization and features an intuitive interface that anyone can learn to master.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=www.xsplit.com">GET Xplit</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>overlay</h3>
+		<ul>
+			<li>
+				<h2>StreamJar</h2>
+				<p>
+					StreamJar is a way for you to handle your tips and alerts in an easy way. thay intergrate with your streaming service to play custom sounds or show custom graphics whenever you have a follower, subscriber or receive a tip! This can be easily configured from our website.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=streamjar.tv">GET StreamJar</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>Beam Interactive</h3>
+		<ul>
+			<li>
+				<h2>Beam Sounly Interactive</h2>
+				<p>
+					Beam Sounly Interactive is a interactive sound board for beam.pro have some trolly sounds for your viewers
+				</p>
+				<a href="http://badatgaming.com/go.php?url=streamjar.tv">GET Beam Sounly Interactive</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>Beam Bots</h3>
+		<ul>
+			<li>
+				<h2>ScottyBot</h2>
+				<p>
+					Scottybot is Beam.Pro's premier chat bot, offering an extensive list of features, many of which are not seen elsewhere. It hosts the typical features.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=scottybot.net">
+				GET Scottybot
+				</a>
+			</li>
+			<li>
+				<h2>BlipBot</h2>
+				<p>
+					Blipbot is a bot for beam thats has Points system , Custom commands , Raffles and giveaways , Warning system by the guys from Streamjar 
+				</p>
+				<a href="http://badatgaming.com/go.php?url=getblipbot.com">
+				GET Blipbot
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>Twitch Bots</h3>
+		<ul>
+			<li>
+				<h2>MooBot</h2>
+				<p>
+					Moobot is a Twitch moderator bot that moderates your chat without the need for your constant interaction and attention.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=twitch.moobot.tv">
+				GET MooBot
+				</a>
+			</li>
+			<li>
+				<h2>NightBot</h2>
+				<p>
+					Nightbot is a chat bot for Twitch and YouTube that allows you to automate your live stream's chat with moderation and new features
+				</p>
+				<a href="http://badatgaming.com/go.php?url=beta.nightbot.tv">
+				GET NightBot
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>Discord Bots</h3>
+		<ul>
+			<li>
+				<h2>Mee6</h2>
+				<p>
+					Introducing Mee6 the Discord bot, Add some !skill to your Discord server
+				</p>
+				<a href="http://badatgaming.com/go.php?url=mee6.xyz">
+				GET Mee6
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3>Twitter</h3>
+		<ul>
+			<li>
+				<h2>TweetDeck</h2>
+				<p>
+					TweetDeck is a social media dashboard application for management of Twitter accounts.
+				</p>
+				<a href="http://badatgaming.com/go.php?url=twitch.moobot.tv">
+				GET TweetDeck
+				</a>
+			</li>
+		</ul>
+	</div>
+
+</div>
